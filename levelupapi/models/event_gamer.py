fd3c4c8678a6e7da0d81363a4ef3318c58134b0e
luciidlou <lewisale96@gmail.com>
2022-01-31 13:39:53 -0600

    first commit

---
+++ levelupapi/models/event_gamer.py
@@ -0,0 +1,9 @@
+from django.db import models
+from levelupapi.models.event import Event
+
+from django.db import models
+
+
+class EventGamer(models.Model):
+    gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
+    event = models.ForeignKey("Event", on_delete=models.CASCADE)
